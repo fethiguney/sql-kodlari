9938ccea83062c81f250dcd36f9bb3fbcd447ecf
Fethi <fethiguneyy@gmail.com>
2022-08-29 23:22:14 +0300

    29/08/22

---
+++ SQL-practice-2.sql
@@ -0,0 +1,50 @@
+CREATE TABLE workers
+(
+  id int,
+  name varchar(20),
+  title varchar(60),
+  manager_id int
+);
+INSERT INTO workers VALUES(1, 'Ali Can', 'Dev', 2);
+INSERT INTO workers VALUES(2, 'John Davis', 'QA', 3);
+INSERT INTO workers VALUES(3, 'Angie Star', 'Dev Lead', 4);
+INSERT INTO workers VALUES(4, 'Amy Sky', 'CEO', 5);
+
+select * from workers;
+
+-- Tabloya company_industry isminde sütun ekleyiniz
+alter table workers add column company_industry VARCHAR(30);
+
+-- TABLOYA worker_address sütunu ve defaullt olarakta 'Miami, FL, USA' adresini ekleyiniz.
+alter table workers
+add worker_adress VARCHAR(30)
+DEFAULT 'Miami, FL, USA';
+
+-- tablodaki worker_address sütununu siliniz
+alter table workers 
+DROP column worker_adress;
+
+-- Tablodaki company_industry sütununu, company_profession olarak değiştiriniz.
+alter table workers RENAME company_industry to company_profession
+
+select * from employees;
+
+-- Tablonun ismini employees olarak değişitiriniz
+alter table workers rename to employees;
+
+select * from employees;
+
+-- Tablodaki title sütununa data tipini VARCHAR(50) olarak değiştiriniz.
+alter table employees 
+alter COLUMN title TYPE varchar(50);
+
+-- Tablodaki title sütununa "UNIQUE" kıstlaması ekleyiniz.
+alter table employees
+add constraint title UNIQUE(title)
+
+
+
+
+
+
+
